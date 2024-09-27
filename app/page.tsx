5d755f2901158db84ec9f678456c51d1dcc35ada
Stefan Tapescu <stefan@younichoice.com>
2024-09-27 13:34:37 +0300

    update 2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from 'next/link'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const questions = [
   {
